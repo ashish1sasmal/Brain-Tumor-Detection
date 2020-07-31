de569255aff335a0820b4dac95d766911080c4ed
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-31 18:29:27 +0530

    GIF/Video

--- tumor_detect.py
+++ tumor_detect.py
@@ -3,63 +3,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imutils
 from imutils import perspective
+import sys
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+
+
+def nothing(x):
+    pass
 
 def tumor_part(cnt,area,solidity):
-    if solidity>0.4 and area>2000:
+    if solidity>0.7 and area>2000:
         return True
     else:
         return False
 
-img = cv2.imread("Datasets/tumors/tumor6.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-gray = clahe.apply(gray)
-blur = cv2.GaussianBlur(gray,(5,5),0)
+def process(img3,b):
+    gray = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+    gray = clahe.apply(blur)
+    # gray = clahe.apply(gray)
+    ret,thresh = cv2.threshold(gray,b,255,cv2.THRESH_BINARY)
+    # thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,2)
+    kernel = np.ones((10,10),np.uint8)
+    erosion = cv2.erode(thresh,kernel,iterations = 1)
+    dilation = cv2.dilate(erosion,kernel,iterations = 1)
+    #
+    # opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    # edges = cv2.Laplacian(dilation,cv2.CV_64F)
 
-ret,thresh = cv2.threshold(gray,145,255,cv2.THRESH_BINARY)
-kernel = np.ones((6,6),np.uint8)
-erosion = cv2.erode(thresh,kernel,iterations = 1)
-dilation = cv2.dilate(erosion,kernel,iterations = 1)
-#
-# opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-# edges = cv2.Laplacian(dilation,cv2.CV_64F)
-
-cnts = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-
-
-sol = []
-contours=[]
-#
-# img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-dilation = cv2.cvtColor(dilation,cv2.COLOR_GRAY2BGR)
-mask = np.ones(img.shape[:2], dtype="uint8") * 255
-
-for (i,c) in enumerate(cnts):
-    area = cv2.contourArea(c)
-    hull = cv2.convexHull(c)
-    hull_area = cv2.contourArea(hull)
-    solidity = float(area)/hull_area
-    sol.append([solidity,area])
-    if tumor_part(c, area, solidity):
-        contours.append(c)
-        print("so",area,solidity)
-        cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
-        cv2.drawContours(dilation, [c], -1, (0, 255, 0), 2)
-    else:
-        cv2.drawContours(mask, [c], -1, 0, -1)
+    cnts = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
 
-dilation = cv2.bitwise_and(dilation, dilation, mask=mask)
+    sol = []
+    contours=[]
+    #
+    # img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    dilation = cv2.cvtColor(dilation,cv2.COLOR_GRAY2BGR)
+    mask = np.ones(img3.shape[:2], dtype="uint8") * 255
 
-plt.subplot(121),plt.imshow(img)
-plt.title('Original')
-plt.subplot(122),plt.imshow(dilation)
-plt.title('Tumor Brain')
-plt.suptitle("Brain Tumor Detection Using Image Processing")
-plt.show()
+    for (i,c) in enumerate(cnts):
+        area = cv2.contourArea(c)
+        hull = cv2.convexHull(c)
+        hull_area = cv2.contourArea(hull)
+        solidity = float(area)/hull_area
+        sol.append([solidity,area])
+        if tumor_part(c, area, solidity):
+            contours.append(c)
+            # print("so",area,solidity)
+            cv2.drawContours(img3, [c], -1, (1,255,11), 2)
+            cv2.drawContours(dilation, [c], -1, (0,0,254), 2)
+        else:
+            cv2.drawContours(mask, [c], -1, 0, -1)
 
-cv2.imwrite("Results/result1.jpg",img)
+
+    dilation = cv2.bitwise_and(dilation, dilation, mask=mask)
+    return (img3,dilation)
+
+
+
+img1 = cv2.imread(f"Datasets/tumors/{sys.argv[1]}")
+img, dilation = process(img1,127)
+res =  np.hstack((img, dilation))
+cv2.imshow("image",res)
+
+cv2.createTrackbar('Intensity','image',0,256,nothing)
+
+while True:
+    b =  cv2.getTrackbarPos('Intensity','image')
+    img, dilation = process(img1,b)
+    res =  np.hstack((img, dilation))
+
+
+    cv2.imwrite("Results/result2.jpg",dilation)
+    cv2.imshow("image",res)
+    img1 = cv2.imread(f"Datasets/tumors/{sys.argv[1]}")
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
 
 # plt.subplot(121),plt.imshow(res)
 # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
@@ -68,7 +92,7 @@ cv2.imwrite("Results/result1.jpg",img)
 # plt.suptitle(methods[0])
 # plt.show()
 
-sol.sort(reverse=True)
+# sol.sort(reverse=True)
 
 
 # for i in range(len(sol)):
@@ -85,4 +109,4 @@ sol.sort(reverse=True)
 # cv2.imshow("Br",img)
 #
 # cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
